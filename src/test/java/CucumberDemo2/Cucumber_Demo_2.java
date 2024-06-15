4c83852b434d955a6aa138d8d069bfc7b1ded228
varsha31889 <varsha31889@gmail.com>
2024-06-15 16:19:33 +0530

    Cucumber_2

--- src/test/java/CucumberDemo2/Cucumber_Demo_2.java
+++ src/test/java/CucumberDemo2/Cucumber_Demo_2.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
